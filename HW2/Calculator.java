56326b3ffa34d176deed0cad44a6d0d7b803b060
rebeccacm <33164964+rebeccacm@users.noreply.github.com>
2019-02-15 14:34:28 -0700

    Initialize project with javadoc comments

---
+++ HW2/Calculator.java
@@ -0,0 +1,73 @@
+//************************************************************************
+// ASU CSE360 Assignment 2
+// Name of Author: Rebecca Martin
+// Class ID: 474
+// Description: 
+//************************************************************************
+
+package cse360assign2;
+
+public class Calculator {
+
+    /**
+     * global variable total holds the result from the calculator computations 
+     */
+	private int total;
+	
+    /**
+     * This method is the constructor.  It initializes total to zero. 
+     */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+    /**
+     * This method returns the total variable
+     */
+	public int getTotal () {
+		return 0;
+	}
+	
+    /**
+     * This method adds the parameter to the total variable
+     * 
+     * @param  value  integer to be added to total
+     */
+	public void add (int value) {
+		
+	}
+	
+    /**
+     * This method subtracts the parameter from the total variable
+     * 
+     * @param  value  integer to be subtracted from the total
+     */
+	public void subtract (int value) {
+		
+	}
+	
+    /**
+     * This method multiplies the parameter with the total variable
+     * 
+     * @param  value  integer to be multiplied with total
+     */
+	public void multiply (int value) {
+		
+	}
+	
+    /**
+     * This method divides the total variable by the parameter
+     * 
+     * @param  value  integer to divide total by
+     */
+	public void divide (int value) {
+		
+	}
+	
+    /**
+     * This method returns the previous computations done by the calculator
+     */
+	public String getHistory () {
+		return "";
+	}
+}
